39f521ac47f4e5c3efcf3de6dc90c9f806022eee
Matteo Molnar <matteo.molnar@gmail.com>
2024-11-18 18:21:18 -0700

    Changing icons to match labels

--- app/src/main/java/com/madlore/caloric/ui/food/FoodViewModel.kt
+++ app/src/main/java/com/madlore/caloric/ui/food/FoodViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class FoodViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is food Fragment"
     }
     val text: LiveData<String> = _text
 }
